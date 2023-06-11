a46c4682ce4c1818e567eaf5e113779137694e64
Vedant Valia <vedantvalia@Vedants-Laptop.local>
2023-06-12 00:56:07 +0530

    Adding the all the codes here

---
+++ service-1/server.js
@@ -0,0 +1,44 @@
+// server.js
+console.log('May Node be with you');
+const express = require('express');
+const app = express();
+const ampq = require('amqplib');
+var connection, channel;
+connect();
+async function connect (){
+    try{
+        const ampqserver="amqp://localhost:5672";
+        connection= await ampq.connect(ampqserver);
+        channel = await connection.createChannel();
+        await channel.assertQueue('rabbit');
+
+    } catch(err){
+        console.log(err);
+    }
+
+}
+
+app.get('/send',(req,res) => {
+  const fakedata={
+    name: "vedant",
+    company : "spaceX"
+  };
+  channel.sendToQueue("rabbit",Buffer.from(JSON.stringify(fakedata)));
+  // await channel.close();
+  // await connection.close();  
+  return res.send("Done");
+}) 
+
+
+app.listen(5001, function () {
+    console.log('listening on 5001');
+  });
+
+
+app.get('/',(req, res) => {
+    //res.send('Hello World')
+    res.sendFile(__dirname + '/index.html');
+        // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+        // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+      
+    });
---
+++ service-2/server.js
@@ -0,0 +1,42 @@
+// server.js
+console.log('May Node be with you');
+const express = require('express');
+const app = express();
+const ampq = require('amqplib');
+connect();
+var connection, channel 
+async function connect (){
+    try{
+        const ampqserver="amqp://localhost:5672";
+        connection= await ampq.connect(ampqserver);
+        channel = await connection.createChannel();
+        await channel.assertQueue('rabbit');
+
+        channel.consume('rabbit',data=>{
+            console.log(`Recieved ${Buffer.from(data.content)}`);
+
+        }, {
+            noAck: true
+        });
+
+    } catch(err){
+        console.log(err);
+    }
+
+}
+app.listen(5002, function () {
+    console.log('listening on 5002')
+  })
+
+app.get('/send',(req,res) => {
+    res.send("Hello world")
+})  
+
+
+app.get('/',(req, res) => {
+    //res.send('Hello World')
+    res.sendFile(__dirname + '/index.html')
+        // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+        // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+      
+    })
